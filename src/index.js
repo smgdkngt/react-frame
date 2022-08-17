724222ec6945f76761b807b1cc113289363d11d1
Sem Goedknegt <sem@ivaldi.nl>
2022-08-17 16:02:26 +0200

    first version of chakra bridge

--- src/index.js
+++ src/index.js
@@ -1,26 +1,93 @@
-import { ColorModeScript } from '@chakra-ui/react';
-import React, { StrictMode } from 'react';
+import React from 'react';
 import * as ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import * as serviceWorker from './serviceWorker';
+import { ChakraProvider, theme } from '@chakra-ui/react';
+import Bridge from './Bridge';
 
-const container = document.getElementById('root');
-const root = ReactDOM.createRoot(container);
+customElements.define('react-frame',
+  class extends HTMLElement {
+    constructor() {
+      super();
+    }
 
-root.render(
-  <StrictMode>
-    <ColorModeScript />
-    <App />
-  </StrictMode>
+    connectedCallback(){
+      setTimeout(()=> {
+        let doc = new DOMParser().parseFromString(this.innerHTML, "text/html");
+        let components = doc.documentElement.childNodes[1].childNodes;        
+        let output = this.loopNodelist(components);
+
+        console.log(output);
+        this.innerHTML = `<div data-root></div>`;
+
+        const container = this.querySelector('[data-root]');
+        const root = ReactDOM.createRoot(container);
+
+        root.render(
+          <ChakraProvider theme={theme}>
+            <Bridge contents={output} />
+          </ChakraProvider>
+        );        
+      })
+    }
+
+    loopNodelist(collection) {
+      let output = [];
+
+      for (let i = 0; i < collection.length; i++) {
+        output.push(this.objectFromNode(collection.item(i)));
+      }
+
+      return output;
+    }
+
+    objectFromNode(node) {
+      if (node.localName) {
+        let component = {
+          type: 'component',
+          component: this.handleName(node.localName),
+          props: this.propsFromNode(node.attributes),
+        }
+
+        if (node.childNodes.length) {
+          component.children = this.loopNodelist(node.childNodes);
+        }
+        
+        return component;
+      } else {
+        return {
+          type: 'text',
+          text: node.textContent
+        }
+      }
+    }
+
+    propsFromNode(attributes) {
+      let output = {};
+
+      for (let i = 0; i < attributes.length; i++) {
+        let attr = this.handleName(attributes[i].nodeName, false);
+        if (attributes[i].nodeValue.startsWith('{')) {
+          output[attr] = JSON.parse(attributes[i].nodeValue);
+        } else {
+          output[attr] = attributes[i].nodeValue;
+        }
+      }
+
+      return output;
+    }
+
+    handleName(str, startWithCap = true) {
+      let output = str;
+      output = output.replace('rm-', '');
+
+      if (startWithCap) {
+        output = output.charAt(0).toUpperCase() + output.slice(1);
+      }
+
+      if (!str.startsWith('data-')) {
+        output = output.split('-').reduce((a, b) => a + b.charAt(0).toUpperCase() + b.slice(1));
+      }
+
+      return output;
+    }
+  }
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorker.unregister();
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
